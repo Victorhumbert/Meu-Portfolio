b3eae1f75a7eec0cc1f02c3f72c99708b0ad4183
Victorhumbert <tormentavictor@gmail.com>
2023-11-11 14:13:48 -0300

    Adicionando API github

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("ola")
+async function ApiGithub(){
+    const responseApi = await fetch("https://api.github.com/users/victorhumbert/repos");
+    const dadosJSON =  await responseApi.json();
+
+    await dadosJSON.forEach(itens => {
+        console.log(itens.downloads_url)
+    });
+}
+// blob/main/capa.png
