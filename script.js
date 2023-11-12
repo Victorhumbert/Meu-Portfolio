10f5c5b9c1a3a66837226ee1f03bc1c6cdd70bff
Victorhumbert <tormentavictor@gmail.com>
2023-11-11 23:28:24 -0300

    Testando api

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ async function ApiGithub(){
     const responseApi = await fetch("https://api.github.com/users/victorhumbert/repos");
     const dadosJSON =  await responseApi.json();
 
-    await dadosJSON.forEach(itens => {
-        console.log(itens.downloads_url)
-    });
+    const lekaRepositorio = await dadosJSON[6];
+
+    console.log(lekaRepositorio)
+
+    // await dadosJSON.forEach(itens => {
+    //     console.log(itens[6])
+    // });
 }
 // blob/main/capa.png
+
+// https://raw.githubusercontent.com/Victorhumbert/LekaCorretora.github.io/main/capa.png
+
+// https://github.com/Victorhumbert/LekaCorretora.github.io/blob/main/capa.png
