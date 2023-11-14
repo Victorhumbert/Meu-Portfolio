6469afdf62c169e67d31b896c6d7dc97b36b31b4
Victorhumbert <tormentavictor@gmail.com>
2023-11-14 00:13:34 -0300

    Api consumida com sucesso, talvez precise ajeitar algumas medidas das IMGs mas es´ta tudo funcionando certinho, agora falta fazer o formulario funcionar

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
-async function ApiGithub(){
+async function ApiGithub() {
     const user = "Victorhumbert";
-    const responseApi = await fetch(`https://api.github.com/users/${user}/repos`);
-    const dadosJSON =  await responseApi.json();
-
-    const listaRepositorios = await dadosJSON.forEach(repositório => {
-        const nome = repositório.name;
-        const urlImg = `https://raw.githubusercontent.com/${user}/${nome}/main/capa.png`;
-        const urlSite = `https://${user}.github.io/${nome}/`;
-        const urlRepositorio = repositório.html_url;
+    try{
+        const responseApi = await fetch(`https://api.github.com/users/${user}/repos`);
         
-async function initProjetos(){
-    const listaProjetos = document.querySelector(".lista-projetos");
-    function criarLista(){
-        const criarElemento = document.createElement("li");
-        criarElemento.innerHTML = `<img src="${urlImg}" alt="${nome}" width="520" height="320" class="img-projeto">
-        <div>
-            <a href="${urlSite}" target="_blank" class="url-pagina">Página</a>
-            <a href="${urlRepositorio}" target="_blank" class="">Repositório</a>
-        </div>`;
-        return criarElemento;
+        if (!responseApi.ok) {
+            throw new Error(`Erro ao obter dados da API. Status: ${responseApi.status}`);
+        }
+        const dadosJSON = await responseApi.json();
+        const listaRepositorios = dadosJSON;
+
+        async function initProjetos() {
+            const listaProjetos = document.querySelector(".lista-projetos");
+
+            listaRepositorios.forEach(async (repositorio) => {
+                const nome = repositorio.name;
+                const urlImg = `https://raw.githubusercontent.com/${user}/${nome}/main/capa.png`;
+                const urlSite = `https://${user}.github.io/${nome}/`;
+                const urlRepositorio = repositorio.html_url;
+
+                function criarLista() {
+                    const criarElemento = document.createElement("li");
+                    criarElemento.setAttribute("class", "item-lista");
+                    criarElemento.innerHTML = `<img src="${urlImg}" alt="${nome}" width="520" height="320" class="img-projeto">
+                        <div>
+                            <a href="${urlSite}" target="_blank" class="url-pagina">Página</a>
+                            <a href="${urlRepositorio}" target="_blank" class="">Repositório</a>
+                        </div>`;
+                    listaProjetos.appendChild(criarElemento);
+                }
+
+                criarLista();
+            });
+        }
+
+        await initProjetos();
+    } catch(error) {
+        console.error("Erro durante a requisição:", error.message);
     }
-    criarLista()
-    }
-    initProjetos()
-    });
 }
 
-
+ApiGithub()
